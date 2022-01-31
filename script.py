8b3928d006a6766340fb246dd08407a18f74ac7a
LEGEND-AI <97836367+LEGEND-AI@users.noreply.github.com>
2022-01-31 09:29:21 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,31 @@
+class Script(object):
+
+    START_MSG = """<b>Hy {},
+
+Im a simple bot which is designed and built for adding unlimited filters in any group
+Hit <i>/help</i> for more information.</b>
+"""
+
+
+    HELP_MSG = """
+<b>What is a filter bot?;</b>
+
+<i>A bot were group admins can set replies for a particular keyword and the bot will automatically send preset replies whenever that keyword enountered in the chat.</i>
+
+/start - <code>Check if I'm alive!</code>
+/help - <code>Command help</code>
+/about -<code>Something about me!</code>
+
+@LegendBot_XD
+"""
+
+
+    ABOUT_MSG = """⭕️<b>My Name : Filter Bot</b>
+
+⭕️<b>Creater :</b> @LegendBoy_XD 
+
+⭕️<b>Language :</b> <code>Python3</code>
+
+⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+
+"""
